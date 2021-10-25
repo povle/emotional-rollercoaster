38969c02c78eb9fa2dc5e1621d405991d56ff2da
povle <pasha@blinov.co>
2021-10-25 22:16:33 +0300

    score only own messages by default and add peer_id filter

--- score.py
+++ score.py
@@ -16,7 +16,9 @@ def download_model(model: str):
     destination_path: str = os.path.join(DATA_BASE_PATH, destination)
     if os.path.exists(destination_path):
         return
+    print('Загрузка языковой модели...')
     downloader.download(source=source, destination=destination)
+    print('Загрузка завершена.')
 
 
 download_model('fasttext-social-network-model')
@@ -26,10 +28,20 @@ model = FastTextSocialNetworkModel(tokenizer=RegexTokenizer())
 @click.command()
 @click.argument('data_path', type=click.Path(dir_okay=False, path_type=Path))
 @click.argument('save_path', type=click.Path(dir_okay=False, path_type=Path))
-@click.option('-s', '--sender', help='id отправителя сообщения, например 12345 или -12345 для сообществ. Для анализа только собственных сообщений -s self')
-def score(data_path: Path, save_path: Path, sender: str = None):
+@click.option('-p',
+              '--peer_id',
+              help='id беседы сообщения, для личных диалогов равен id другого человека')
+@click.option('-s',
+              '--sender',
+              default='self',
+              help='id отправителя сообщения, например 12345 или -12345 для сообществ.'
+                   'По умолчанию анализируются только собственные сообщения,'
+                   'чтобы проанализировать все используйте -s all')
+def score(data_path: Path, save_path: Path, peer_id: str = None, sender='self'):
     with data_path.open(encoding='utf8') as f:
-        messages = [x for x in json.loads(f.read()) if sender is None or x['sender'] == sender]
+        messages = [x for x in json.loads(f.read())
+                    if (sender == 'all' or x['sender'] == sender)
+                    and (peer_id is None or x['peer_id'] == peer_id)]
 
     scores = {}
     for msg in tqdm(messages):

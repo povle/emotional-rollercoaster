c4d42e2433c5f3778cd69240dac0a2894a6cb73a
povle <pasha@blinov.co>
2022-02-10 02:01:40 +0300

    switch to pandas

--- plot.py
+++ plot.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
-import datetime
-import numpy as np
 import click
+import pandas as pd
 from pathlib import Path
 from typing import Tuple
 
 
-def moving_average(x, w):
-    return np.convolve(x, np.ones(w), 'valid') / w
+def load_scores(path: Path) -> pd.DataFrame:
+    df = pd.read_csv(path, names=['datetime', 'score'], index_col='datetime')
+    df.index = pd.to_datetime(df.index, unit='s')
+    return df
 
 
 @click.command()
 @click.argument('data_paths', nargs=-1, type=click.Path(exists=True, dir_okay=False, path_type=Path))
 @click.option('-w', '--width', type=int, default=5000)
 def plot_moving_average(data_paths: Tuple[Path], width: int):
+    single = len(data_paths) == 1
+
+    ax = plt.gca()
+    ax.xaxis.label.set_visible(False)
+
     for data_path in data_paths:
-        raw_x = []
-        raw_y = []
-        with data_path.open() as f:
-            for line in f:
-                a, b = line.split(',')
-                raw_x.append(int(a))
-                raw_y.append(float(b))
+        df = load_scores(data_path)
+        ax = df.rolling(width).mean().plot(ax=ax, legend=False)
 
-        x = [datetime.datetime.fromtimestamp(x) for x in moving_average(raw_x, width)]
-        y = moving_average(raw_y, width)
-
-        plt.plot(x, y)
-
-    if len(data_paths) == 1:
-        plt.axhline(y=sum(y)/len(y), color='r')
+    if single:
+        plt.axhline(y=df['score'].mean(), color='r')
     plt.show()
 
 
